56f02b65170e88b8930cda1ed555f2ea8b9c37e3
Ilham Fajri <46326047+Hamuniverse@users.noreply.github.com>
2025-04-25 20:02:58 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class program
+{
+    public void main(string[] args)
+    {
+        Console.WriteLine("1. Lihat Saldo");
+        Console.WriteLine("2. Tarik Tunai");
+        Console.WriteLine("3. Setor Tunai");
+        Console.WriteLine("4. Transfer Antar Rekening");
+        Console.WriteLine("5. Keluar");
+        Console.WriteLine("SILAHKAN PILIH MENU");
+    }
+
+    class menu
+    {
+        private string NamaNasabah;
+        private string NoRekening;
+        private double Saldo;
+    }
+}
